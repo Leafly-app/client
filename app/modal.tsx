76c1b7374c3240e86a478ba1bdc3cfadb0905089
hyeseong <tell280210@gmail.com>
2025-09-17 13:22:54 +0900

    chore: 데모 파일 제거

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,13 @@
 import { Link } from "expo-router";
-import { StyleSheet } from "react-native";
-
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
+import { Text, View } from "react-native";
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-lg mb-4">Modal Screen</Text>
+      <Link href="/" dismissTo>
+        <Text className="text-blue-500">Go to home screen</Text>
       </Link>
-    </ThemedView>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
