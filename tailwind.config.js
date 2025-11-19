b5758b8bfb9cd47621d916a557f6b76d052ed318
SangwanYu <ryusangwan12@gmail.com>
2025-11-19 20:53:30 +0900

    design: add colors, font system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { colors } from "./styles/colors";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,7 +10,37 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: { ...colors },
+      fontFamily: {
+        sans: ["Pretendard Variable", "Arial", "sans-serif"],
+      },
+      fontSize: {
+        "heading-24-bold": ["24px", { lineHeight: "36px", fontWeight: "700" }],
+        "heading-24-semibold": ["24px", { lineHeight: "36px", fontWeight: "600" }],
+        "heading-24-regular": ["24px", { lineHeight: "36px", fontWeight: "400" }],
+        "heading-20-bold": ["20px", { lineHeight: "28px", fontWeight: "700" }],
+        "heading-20-semibold": ["20px", { lineHeight: "28px", fontWeight: "600" }],
+        "heading-20-regular": ["20px", { lineHeight: "28px", fontWeight: "400" }],
+
+        "body-16-bold": ["16px", { lineHeight: "24px", fontWeight: "700" }],
+        "body-16-semibold": ["16px", { lineHeight: "24px", fontWeight: "600" }],
+        "body-16-regular": ["16px", { lineHeight: "24px", fontWeight: "400" }],
+        "body-14-bold": ["14px", { lineHeight: "20px", fontWeight: "700" }],
+        "body-14-semibold": ["14px", { lineHeight: "20px", fontWeight: "600" }],
+        "body-14-medium": ["14px", { lineHeight: "20px", fontWeight: "500" }],
+        "body-14-regular": ["14px", { lineHeight: "20px", fontWeight: "400" }],
+        "body-12-bold": ["12px", { lineHeight: "18px", fontWeight: "700" }],
+        "body-12-semibold": ["12px", { lineHeight: "18px", fontWeight: "600" }],
+        "body-12-regular": ["12px", { lineHeight: "18px", fontWeight: "400" }],
+        "body-10-bold": ["10px", { lineHeight: "16px", fontWeight: "700" }],
+        "body-10-semibold": ["10px", { lineHeight: "16px", fontWeight: "600" }],
+        "body-10-regular": ["10px", { lineHeight: "16px", fontWeight: "400" }],
+        "body-8-bold": ["8px", { lineHeight: "16px", fontWeight: "700" }],
+        "body-8-semibold": ["8px", { lineHeight: "16px", fontWeight: "600" }],
+        "body-8-regular": ["8px", { lineHeight: "16px", fontWeight: "400" }],
+      },
+    },
   },
   plugins: [],
 };
