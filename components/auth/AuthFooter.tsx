affe43f1a2c848c25d2bf6c701454f9effe25f38
SangwanYu <ryusangwan12@gmail.com>
2025-11-20 09:46:37 +0900

    refactor: 중복 이벤트 처리 태그 삭제

--- components/auth/AuthFooter.tsx
+++ components/auth/AuthFooter.tsx
@@ -1,6 +1,6 @@
 import { Href, Link } from "expo-router";
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 
 interface AuthFooterProps {
   text: string;
@@ -13,9 +13,7 @@ export default function AuthFooter({ text, linkText, href }: AuthFooterProps) {
     <View className="flex-row justify-center mt-4">
       <Text className="text-gray-500">{text} </Text>
       <Link href={href}>
-        <TouchableOpacity>
-          <Text className="text-primary-600 font-bold">{linkText}</Text>
-        </TouchableOpacity>
+        <Text className="text-primary-600 font-bold">{linkText}</Text>
       </Link>
     </View>
   );
