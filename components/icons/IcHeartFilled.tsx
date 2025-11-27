24a1584a60ee7e5ee97ac29597833cb4f9c7a854
SangwanYu <ryusangwan12@gmail.com>
2025-11-28 01:22:45 +0900

    style: 코드 포맷 수정

--- components/icons/IcHeartFilled.tsx
+++ components/icons/IcHeartFilled.tsx
@@ -2,7 +2,14 @@ import * as React from "react";
 import Svg, { Path } from "react-native-svg";
 import type { SvgProps } from "react-native-svg";
 const SvgIcHeartFilled = (props: SvgProps) => (
-  <Svg xmlns="http://www.w3.org/2000/svg" width={14} height={13} viewBox="0 0 14 13" fill="none" {...props}>
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={14}
+    height={13}
+    viewBox="0 0 14 13"
+    fill="none"
+    {...props}
+  >
     <Path
       strokeLinecap="round"
       strokeLinejoin="round"
