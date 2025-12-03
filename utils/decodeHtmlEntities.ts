0fc0563d4d8fc1e9ba2b8160d24de26f1ac2f799
SangwanYu <ryusangwan12@gmail.com>
2025-12-03 15:23:45 +0900

    fix: html 엔티티 디코딩 적용

---
+++ utils/decodeHtmlEntities.ts
@@ -0,0 +1,13 @@
+export function decodeHtmlEntities(text: string): string {
+  const entities: Record<string, string> = {
+    "&amp;": "&",
+    "&lt;": "<",
+    "&gt;": ">",
+    "&quot;": '"',
+    "&#39;": "'",
+    "&apos;": "'",
+    "&nbsp;": " ",
+  };
+
+  return text.replace(/&[#\w]+;/g, (entity) => entities[entity] || entity);
+}
