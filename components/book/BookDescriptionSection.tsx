75869d058910fa187b1a603db6706b599b94bd9a
Sangwan Yu <163666284+wantkdd@users.noreply.github.com>
2025-12-03 15:44:09 +0900

    [Design/#40] html 엔티티 디코딩 - 책 상세 화면 수정 (#41)
    * design: 도서 상세 바텀시트 디자인 수정
    
    * fix: html 엔티티 디코딩 적용
    
    * refactor: he 라이브러리 사용으로 변경

--- components/book/BookDescriptionSection.tsx
+++ components/book/BookDescriptionSection.tsx
@@ -1,3 +1,4 @@
+import he from "he";
 import { Text, View } from "react-native";
 import IcComment from "@/components/icons/IcComment";
 import { colors } from "@/styles/colors";
@@ -7,13 +8,15 @@ interface BookDescriptionSectionProps {
 }
 
 export default function BookDescriptionSection({ description }: BookDescriptionSectionProps) {
+  const decodedDescription = he.decode(description);
+
   return (
     <View className="bg-secondary-50 rounded-lg py-5 px-4 shadow-md">
       <View className="flex-row items-center gap-2 mb-3">
         <IcComment width={16} height={16} stroke={colors.secondary[700]} />
         <Text className="text-body-14-semibold text-gray-900">책 소개</Text>
       </View>
-      <Text className="text-body-12-regular text-gray-700">{description}</Text>
+      <Text className="text-body-12-regular text-gray-700">{decodedDescription}</Text>
     </View>
   );
 }
