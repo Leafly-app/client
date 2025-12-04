27ba7ed41c1f6aaa7822fe2281048ff9681e9d86
SangwanYu <ryusangwan12@gmail.com>
2025-12-04 09:34:10 +0900

    chore: 타이포그래피 설정 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,117 +22,117 @@ module.exports = {
     },
   },
   plugins: [
-    plugin(({ addUtilities }) => {
+    plugin(({ addUtilities, theme }) => {
       addUtilities({
         ".text-heading-24-bold": {
           fontSize: "1.5rem",
           lineHeight: "2.25rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-heading-24-semibold": {
           fontSize: "1.5rem",
           lineHeight: "2.25rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-heading-24-regular": {
           fontSize: "1.5rem",
           lineHeight: "2.25rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-heading-20-bold": {
           fontSize: "1.25rem",
           lineHeight: "1.75rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-heading-20-semibold": {
           fontSize: "1.25rem",
           lineHeight: "1.75rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-heading-20-regular": {
           fontSize: "1.25rem",
           lineHeight: "1.75rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-body-16-bold": {
           fontSize: "1rem",
           lineHeight: "1.5rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-body-16-semibold": {
           fontSize: "1rem",
           lineHeight: "1.5rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-body-16-regular": {
           fontSize: "1rem",
           lineHeight: "1.5rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-body-14-bold": {
           fontSize: "0.875rem",
           lineHeight: "1.25rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-body-14-semibold": {
           fontSize: "0.875rem",
           lineHeight: "1.25rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-body-14-medium": {
           fontSize: "0.875rem",
           lineHeight: "1.25rem",
-          fontFamily: "Pretendard-Medium",
+          fontFamily: theme("fontFamily.pretendard-medium")[0],
         },
         ".text-body-14-regular": {
           fontSize: "0.875rem",
           lineHeight: "1.25rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-body-12-bold": {
           fontSize: "0.75rem",
           lineHeight: "1.125rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-body-12-semibold": {
           fontSize: "0.75rem",
           lineHeight: "1.125rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-body-12-regular": {
           fontSize: "0.75rem",
           lineHeight: "1.125rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-body-10-bold": {
           fontSize: "0.625rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-body-10-semibold": {
           fontSize: "0.625rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-body-10-regular": {
           fontSize: "0.625rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
         ".text-body-8-bold": {
           fontSize: "0.5rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-Bold",
+          fontFamily: theme("fontFamily.pretendard-bold")[0],
         },
         ".text-body-8-semibold": {
           fontSize: "0.5rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-SemiBold",
+          fontFamily: theme("fontFamily.pretendard-semibold")[0],
         },
         ".text-body-8-regular": {
           fontSize: "0.5rem",
           lineHeight: "1rem",
-          fontFamily: "Pretendard-Regular",
+          fontFamily: theme("fontFamily.pretendard-regular")[0],
         },
       });
     }),
